105df59b725f57cb13d32fb72f9c58b7ff6b77c0
yrshrivastav <yrshrivastav42+github@gmail.com>
2024-05-20 17:52:14 +0530

    commit

---
+++ LeetCode/LC_Q1.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class LC_Q1 {
+     public static void main(String[] args) {
+        int num1[] = {1,3,5,2,4};
+        int num2[] = {9,4,7};
+
+        int m = num1.length;
+        int n = num2.length;
+        int sizeOfNewArrayNum1 = m+n;
+    
+        num1 = Arrays.copyOf(num1, sizeOfNewArrayNum1);
+        for(int no2 : num2){
+            num1[m] = no2;
+            m++;
+        }
+
+            
+        for(int i=0; i<m; i++){
+            for(int j=i+1; j<m; j++){
+                if(num1[i]>num1[j]){
+                    int temp=num1[i];
+                    num1[i]=num1[j];
+                    num1[j]=temp;
+                }
+            }
+        }
+
+        for(int no3 : num1){
+            System.out.println("No in num1 are : "+no3);
+        }
+
+
+    }
+}
